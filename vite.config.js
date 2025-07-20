5c50f3d7a478c0f0b518b7a584a14d65387f18a5
Basicgit <god0999333@gmail.com>
2025-07-20 18:28:55 +0530

    PWA Updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-     tailwindcss(),
+  plugins: [react(),tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'GOD',
+        short_name: 'GOD',
+        start_url: '/',
+        display: 'standalone',
+         background_color: '#dbeafe',  // Tailwind 'blue-100' for light background
+        theme_color: 'red',       // Tailwind 'indigo-600' to match your button
+
+        icons: [
+          {
+            src: '/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      }
+    })
   ],
 })
